dd6cf58cda19cd54618dcdb3459152c446530324
Nikitha Anthony Raju <nanthonyraju@apple.com>
2019-07-09 10:55:49 +0530

    Variable.js

---
+++ basic01/variables.js
@@ -0,0 +1,20 @@
+let name = 'Nikitha';
+console.log("My name is " + name);
+
+//Number within quotes cannot be added.
+let num = '100';
+let num1 = '2.2';
+let total = num + num1;
+console.log("String Numbers = " + total);
+
+//Reusing existing variable
+num = 100;
+num1 = 2.2;
+total = num + num1;
+console.log("Numbers = " + total);
+
+//Concating String
+let firstName = 'Nikitha';
+let lastName = 'Anthony';
+console.log("Full Name - " + firstName + " " + lastName);
+console.log("Full Name - " + firstName + "_" + lastName);
